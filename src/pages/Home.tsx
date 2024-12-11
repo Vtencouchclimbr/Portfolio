326f423ed83a7cdf9000af0a687f6c775a5bf72c
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-12-11 15:11:30 -0700

    new styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,60 @@
-
-
-import MediaTab from '../components/MediaTab';
-import myImage from '../utils/me3.png';
-import mountain from '../utils/mountain.png';
-import './Home.css';
+import MediaTab from "../components/MediaTab";
+import myImage from "../utils/me3.png";
+import mountain from "../utils/mountain.png";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div className="d-flex flex-column w-100"
+    <div
+      className="d-flex flex-column w-100"
       style={{
-        minHeight: '100vh',
+        minHeight: "100vh",
         backgroundImage: `url(${mountain})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
       }}
     >
-      <div className='media-tab'>
-      <MediaTab />
+      <div className="media-tab">
+        <MediaTab />
       </div>
       {/* Main Content */}
       <div className="mainContain container-fluid d-flex flex-column flex-lg-row justify-content-center align-items-center py-5">
-      
         {/* Left Section with Text */}
-        <div className="col-12 col-lg-6 d-flex justify-content-center align-items-start mb-5 mb-lg-0">
+        <div className="col-12 col-lg-6 order-lg-1 mb-4 mb-lg-0">
           <div className="cardContainer d-flex flex-column justify-content-center align-items-center p-3 w-100">
             <h2 className="welcome text-center mb-3">
               Welcome to my portfolio
             </h2>
             <div className="aboutCard card shadow-lg">
               <div className="card-body">
-                <p style={{color:'rgb(244, 245, 247)'}} className="card-text">
-                Hello, my name is Jesse Anderson. Hailing originally from Maryland, my career journey has taken me across various regions of the U.S..<br />
-                I began my professional life as a Field Land Surveyor, which required a significant adjustment when I transitioned to an office role. Over the years, I've climbed the ranks, taking on responsibilities for coordinating and training surveyors, and managing data for production processes. My experience spans Civil Engineering and extends into Structural Engineering, revealing a universal thread among these disciplines: adherence to purpose-specific parameters, client expectation management, standardized nomenclature, and effective inter-team collaboration.<br/>
-                My background has equipped me not just with technical know-how but also with leadership skills.<br />
-                Recently, I've expanded my expertise by completing an intensive six-month full stack development bootcamp, fueling my ambition to delve deeper into software development. I am eager to leverage my diverse experience in an environment where my drive to succeed can flourish.<br />
-                Thank you for considering my portfolio. I look forward to the opportunity to contribute and grow with your team.<br />
--Jesse
-
+                <p
+                  style={{ color: "rgb(244, 245, 247)" }}
+                  className="card-text"
+                >
+                  Hello, my name is Jesse Anderson. Hailing originally from
+                  Maryland, my career journey has taken me across various
+                  regions of the U.S..<br />
+                  I began my professional life as a Field Land Surveyor, which
+                  required a significant adjustment when I transitioned to an
+                  office role. Over the years, I've climbed the ranks, taking on
+                  responsibilities for coordinating and training surveyors, and
+                  managing data for production processes. My experience spans
+                  Civil Engineering and extends into Structural Engineering,
+                  revealing a universal thread among these disciplines:
+                  adherence to purpose-specific parameters, client expectation
+                  management, standardized nomenclature, and effective
+                  inter-team collaboration.<br />
+                  My background has equipped me not just with technical know-how
+                  but also with leadership skills.<br />
+                  Recently, I've expanded my expertise by completing an
+                  intensive six-month full stack development bootcamp, fueling
+                  my ambition to delve deeper into software development. I am
+                  eager to leverage my diverse experience in an environment
+                  where my drive to succeed can flourish.<br />
+                  Thank you for considering my portfolio. I look forward to the
+                  opportunity to contribute and grow with your team.<br />
+                  -Jesse
                 </p>
               </div>
             </div>
@@ -45,7 +62,7 @@ export default function Home() {
         </div>
 
         {/* Right Section with Image */}
-        <div className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+        <div className="col-12 col-lg-6 order-lg-2 text-center">
           <div className="">
             <h1 className="signature mb-3">Jesse Anderson</h1>
             <h2 className="titleText mb-3">
@@ -53,7 +70,7 @@ export default function Home() {
             </h2>
             <img
               className="myPic img-fluid mb-3"
-              style={{ maxWidth: '200px' }}
+              style={{ maxWidth: "200px", margin: "0 auto" }}
               src={myImage}
               alt="Image of the developer"
             />
